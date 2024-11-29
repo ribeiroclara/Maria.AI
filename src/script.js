1e9471fc048d49b1af618a15cdf0d156de1a8105
Clara <ribeiroclara096@gmail.com>
2024-11-29 10:17:37 -0300

    add basic javascript typewrither

--- src/script.js
+++ src/script.js
@@ -1,6 +1,29 @@
-const logo = document.getElementById('logo');
+const ladybug = document.getElementById('ladybug');
+if (ladybug) {
+    ladybug.addEventListener('click', () => {
+        console.log("A joaninha foi clicada!");
+    });
+}
 
-// Alterar cor de fundo ao clicar no logo
-logo.addEventListener('click', () => {
-    document.body.style.backgroundColor = '#fff';
-});
+const answerForm = document.querySelector(".answer-generator-form");
+if (answerForm) {
+    answerForm.addEventListener("submit", generateAnswer);
+} else {
+    console.error("Formulário com a classe 'answer-generator-form' não foi encontrado.");
+}
+
+function generateAnswer(event) {
+    event.preventDefault(); 
+
+    const answerElement = document.querySelector(".answer");
+    if (answerElement) {
+        new Typewriter(answerElement, {
+            strings: "Your answer will be typed here...",
+            autoStart: true,
+            delay: 1,
+            cursor: "",
+        });
+    } else {
+        console.error("Elemento com a classe 'answer' não foi encontrado.");
+    }
+}
